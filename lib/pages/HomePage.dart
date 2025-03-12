9dee74dca482f458b31f616717e89a78b37ea879
DaffanAlfauzi <daffanalfauzi6@upi.edu>
2025-03-12 08:51:06 +0700

    Update HomePage.dart - Tambah Kelompok 19

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -14,6 +14,16 @@ class _HomePageState extends State<HomePage> {
       body: SafeArea(
         child: Column(
           children: [
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Kelompok 19',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
             _buildSearchBar(),
             Expanded(
               child: ListView(
